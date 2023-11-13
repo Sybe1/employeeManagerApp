a34012993fa7c6c8ade64690d38c7d4dfd2071a7
Sybe Aerts <s1144856@student.hsleiden.nl>
2023-11-13 15:47:47 +0100

    Applicatie

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,100 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Employee } from './employee/employee';
+import { EmployeeService } from './employee/employee.service';
+import { HttpErrorResponse } from '@angular/common/http';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'employeeManagerApp';
+
+export class AppComponent implements OnInit {
+  title(title: any) {
+    throw new Error('Method not implemented.');
+  }
+  public employees: Employee[] = [];
+
+  updating = false; 
+
+  
+  employeeToUpdate = {
+    id:0,
+    firstName:"",
+    lastName:"",
+    gender: "",
+    email:"",
+    jobTitle:"",
+    phone:"",
+    imageUrl:"",
+    employeeCode:""
+  }
+
+ 
+
+  constructor(private employeeService:EmployeeService) {
+  }
+
+  ngOnInit() {
+    this.getEmployees();
+  }
+
+
+  public getEmployees(): void{
+    this.employeeService.getEmployees().subscribe(
+    (response: Employee[]) => {
+      this.employees = response;
+    },
+    (error: HttpErrorResponse) => {
+      alert(error.message)
+    }
+    )
+  }
+
+
+  public addEmployee(registerForm: NgForm): void {
+    this.employeeService.addEmployee(registerForm.value).subscribe(
+      (resp) => {
+        console.log(resp);
+        this.getEmployees();
+      },
+      (err) => {
+        console.log(err);
+      }
+    );
+  }
+
+
+  public deleteEmployee(employee: Employee){
+    if(confirm('Are you sure you want to delete this employee?'))
+      this.employeeService.deleteEmployee(employee.id).subscribe(
+        (resp) => {
+          console.log(resp)
+          this.getEmployees();
+        },
+        (err) => {
+          console.log(err);
+        }
+      );
+  }
+
+
+  public editEmployee(employee: Employee){
+    this.employeeToUpdate = employee;
+  }
+
+  public updateEmployee(){
+    this.employeeService.updateEmployee(this.employeeToUpdate).subscribe(
+      (resp) => {
+        console.log(resp)
+        this.getEmployees();
+      },
+      (err) => {
+        console.log(err);
+      }
+    );
+  }
+
+ 
 }
---
+++ src/app/employee/employee.ts
@@ -0,0 +1,11 @@
+export interface Employee{
+    id: number;
+    firstName: string;
+    lastName: string;
+    gender: string;
+    email: string;
+    jobTitle: string;
+    phone: string;
+    imageUrl: string;
+    employeeCode: string;
+}
